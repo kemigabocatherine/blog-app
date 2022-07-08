33d750fe918a2e35801f98bcf4b316f874a1722a
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-07-08 15:36:21 +0300

    Add posts ref to likes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_08_123320) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_08_123520) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_08_123320) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.bigint "posts_id", null: false
+    t.index ["posts_id"], name: "index_likes_on_posts_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
@@ -55,6 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_08_123320) do
   end
 
   add_foreign_key "comments", "users"
+  add_foreign_key "likes", "posts", column: "posts_id"
   add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
 end
