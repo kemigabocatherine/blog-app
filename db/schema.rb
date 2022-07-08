c730de62cbf1151e58944db438ddf3b19fa3cc46
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-07-08 15:33:42 +0300

    Add user ref to like

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_08_123214) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_08_123320) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_08_123214) do
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -53,5 +55,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_08_123214) do
   end
 
   add_foreign_key "comments", "users"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
 end
