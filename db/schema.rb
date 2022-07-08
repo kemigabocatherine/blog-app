87d6b829f25602bb4bd27e12df86fc5a0ff3240b
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-07-08 15:23:00 +0300

    Create like table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_08_122000) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_08_122215) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_08_122000) do
     t.integer "author_id"
     t.integer "post_id"
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "author_id"
+    t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
