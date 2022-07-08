448b35b5b19e8be0a6716f13041127ef72ba184b
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-07-08 15:38:30 +0300

    Add posts ref to comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_08_123520) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_08_123804) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_08_123520) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.bigint "posts_id", null: false
+    t.index ["posts_id"], name: "index_comments_on_posts_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
@@ -56,6 +58,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_08_123520) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts", column: "posts_id"
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts", column: "posts_id"
   add_foreign_key "likes", "users"
