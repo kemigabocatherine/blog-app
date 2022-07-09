14c0a0117cd45681ccf0b77af71b8e752e659caa
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-07-09 20:19:54 +0300

    Add validations for the post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,9 @@ class Post < ApplicationRecord
   has_many :likes
   belongs_to :user, class_name: 'User'
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def recent_comments
     comments.limit(5).order(created_at: :desc)
   end
