9d4ccd74d644e1f6052637acc4d74c4b6581c7fa
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-07-12 02:16:06 +0300

    Add routes to the users and posts pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :users
-  resources :likes
-  resources :posts
-  resources :comments
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "users#index"
+
+  resources :users, only: [:index, :show]
+  resources :posts, only: [:index, :show]
+
 end
