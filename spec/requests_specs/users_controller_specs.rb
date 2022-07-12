3baaea211a800f451634892676300d62db29447f
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-07-12 20:41:24 +0300

    Fix linter errors

--- spec/requests_specs/users_controller_specs.rb
+++ spec/requests_specs/users_controller_specs.rb
@@ -3,32 +3,32 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   describe 'GET #index' do
     before(:each) { get users_path }
-      it 'is a success' do
-        expect(response).to have_http_status(:ok)
-      end
+
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
     end
 
     it 'renders the index template' do
       expect(response).to render_template('index')
     end
 
-    it 'shows the correct text in the response body'
+    it 'shows the correct text in the response body' do
       expect(response.body).to_include('Users#Index')
     end
   end
 
   describe 'GET #show' do
     before(:each) { get show_path }
-      it 'is a success' do
-        expect(response).to have_http_status(:ok)
-      end
+
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
     end
 
     it 'renders the show template' do
       expect(response).to render_template('show')
     end
 
-    it 'shows the correct text in the response body'
+    it 'shows the correct text in the response body' do
       expect(response.body).to_include('Users#Show')
     end
   end
