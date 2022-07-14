facb5b714a376503b878be98ce1d2ea3187142c7
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-07-14 20:31:20 +0300

    Remove commented out code in the migrations

--- db/migrate/20220709152149_reset_default_posts_counter.rb
+++ db/migrate/20220709152149_reset_default_posts_counter.rb
@@ -1,6 +1,5 @@
 class ResetDefaultPostsCounter < ActiveRecord::Migration[7.0]
   def change
     change_column_default :posts, :comments_counter, from: nil, to: 0
-    # change_column_null :posts, :comments_counter, false
   end
 end
