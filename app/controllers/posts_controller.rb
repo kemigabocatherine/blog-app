3027cdc2c9bcb5edb2be310ca8d2de9e860b1106
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-07-16 21:11:03 +0300

    Add method for adding new post, comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,8 +5,28 @@ class PostsController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
-    @user = @post.user
+    @comment = Comment.new
     @comments = @post.comments
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    
+    if @post.save
+      redirect_to user_post_path(current_user, @post)
+    else
+      render :new
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
